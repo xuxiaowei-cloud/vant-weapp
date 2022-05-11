b62a8d3152072ef2ee3d8a1b997675b792d3f5e0
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-11 21:36:08 +0800

    登录

--- app.js
+++ app.js
@@ -6,14 +6,30 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
+    // 登录：https://developers.weixin.qq.com/miniprogram/dev/api/open-api/login/wx.login.html
+    // 后台使用：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          //发起网络请求
+          wx.request({
+            url: this.host + '/onLogin',
+            data: {
+              code: res.code
+            },
+            success: resp => {
+              console.log(resp)
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
       }
     })
   },
   globalData: {
     userInfo: null
-  }
+  },
+  host: 'http://gateway.example.xuxiaowei.cloud:1101/wechat-applet'
 })
