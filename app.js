46b676cf879f24247096d2fad9c451c8e8af271d
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-05-15 23:37:14 +0800

    优化

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ App({
         if (res.code) {
           // 发起网络请求：微信小程序登录
           wx.request({
-            url: this.host + '/onLogin',
+            url: this.wechatHost() + '/onLogin',
             data: {
               code: res.code
             },
@@ -45,7 +45,7 @@ App({
 
                       // 获取 access_token, refresh_token
                       wx.request({
-                        url: this.tokenHost + '&client_id=' + this.client_id + '&client_secret=' + this.client_secret + '&appid=' + this.globalData.appid + '&username=' + this.globalData.openid + '&password=' + res.code,
+                        url: this.tokenHost() + '&appid=' + this.globalData.appid + '&username=' + this.globalData.openid + '&password=' + res.code,
                         method: 'POST',
                         success: res => {
                           console.log('授权结果', res)
@@ -78,11 +78,11 @@ App({
                   }
                 })
               } else {
-                console.error('登录失败！', res.msg)
+                console.error('登录失败！', resData.msg)
 
                 Dialog.alert({
                   title: '登录异常',
-                  message: res.errMsg,
+                  message: resData.msg,
                 }).then(() => {
                   // on close
                 })
@@ -114,10 +114,16 @@ App({
     expires_in: null
   },
   // 小程序服务地址
-  host: 'http://gateway.example.xuxiaowei.cloud:1101/wechat-applet',
+  wechatHost() {
+    return `${this.host}/wechat-applet`
+  },
   // 授权服务地址：授权类型为密码模式
   // 客户端类型：wechat_applet（代表微信小程序）
-  tokenHost: 'http://gateway.example.xuxiaowei.cloud:1101/authorization-server/oauth/token?grant_type=password&client_type=wechat_applet',
+  tokenHost() {
+    return `${this.host}/authorization-server/oauth/token?grant_type=password&client_type=wechat_applet&client_id=${this.client_id}&client_secret=${this.client_secret}`
+  },
+  // 小程序服务地址
+  host: 'http://gateway.example.xuxiaowei.cloud:1101',
   // 密码模式下的客户ID（为了保证安全，请给该用户仅授权：password 模式）
   client_id: 'xuxiaowei_client_wechat_id',
   // 密码模式下的客户凭证
