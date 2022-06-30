b3818f209fe623a72c6d4862fcfcab6884b088e7
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2022-06-30 18:53:56 +0800

    支持 OAuth 2.1 登录

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ App({
                 this.globalData.access_token = access_token
                 // 当且仅当拥有 refresh_token 授权类型时才有该值（如需实时生效，需要清空权限表、刷新权限表）
                 this.globalData.refresh_token = resData.refresh_token
-                this.globalData.jti = resData.jti
                 this.globalData.scope = resData.scope
                 this.globalData.expires_in = Date.now() + resData.expires_in * 1000
+                console.log(new Date(this.globalData.expires_in).toISOString())
               } else {
                 Dialog.alert({
                   title: '授权异常',
@@ -68,31 +68,28 @@ App({
   },
   globalData: {
     userInfo: null,
-    // 用户标识
-    openid: null,
     access_token: null,
     // 当且仅当拥有 refresh_token 授权类型时才有该值（如需实时生效，需要清空权限表、刷新权限表）
     refresh_token: null,
-    jti: null,
     scope: null,
     expires_in: null
   },
   // 小程序服务地址
   wechatHost() {
-    return `${this.host}/wechat-applet`
+    return `${this.host}/wechat_miniprogram`
   },
   // 授权服务地址：授权类型为密码模式
   // 客户端类型：wechat_applet（代表微信小程序）
   tokenHost() {
     // 此处发送的 client_secret 可以进行加密、签名，并在 https://gitee.com/xuxiaowei-cloud/xuxiaowei-cloud/blob/main/authorization-server/src/main/java/cloud/xuxiaowei/authorizationserver/service/impl/ClientPasswordEncoderImpl.java 中进行验证加密、签名
-    return `${this.host}/passport/oauth2/token?grant_type=wechat_applet&client_id=${this.client_id}&client_secret=${this.client_secret}&appid=wxcf4f3a217a8bc728`
+    return `${this.host}/passport/oauth2/token?grant_type=wechat_miniprogram&client_id=${this.client_id}&client_secret=${this.client_secret}&appid=wxcf4f3a217a8bc728`
   },
   // 小程序服务地址
   host: 'http://gateway.example.next.xuxiaowei.cloud:1101',
   // 密码模式下的客户ID（为了保证安全，请给该用户仅授权：password 模式）
-  client_id: 'xuxiaowei_client_wechat_applet_id',
+  client_id: 'xuxiaowei_client_webchat_miniprogram_id',
   // 密码模式下的客户凭证
-  client_secret: 'xuxiaowei_client_wechat_applet_secret',
+  client_secret: 'xuxiaowei_client_webchat_miniprogram_secret',
   // 正常响应代码
   ok: '000000'
 })
